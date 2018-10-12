92cea393eab69a9cb06bd3f20c2c9724dbdb13db
Sergey Patara <upload051@yahoo.com>
2018-10-12 10:41:30 +0300

    ...

--- src/by/upload/sending/Sender.java
+++ src/by/upload/sending/Sender.java
@@ -1,17 +1,12 @@
 package by.upload.sending;
 
-import by.upload.parsing.AddressParser;
-import by.upload.parsing.TextParser;
 import by.upload.utils.Constants;
 import by.upload.utils.Utils;
 
-import java.io.IOException;
 import java.util.List;
 
 public class Sender {
     private Mailer mailer = new Mailer("anon1dvach@gmail.com", "ZAQ!@wsx");
-    //private AddressParser main = new AddressParser();
-    //private TextParser tx = new TextParser();
     private Utils utils = new Utils();
     private String text;
     private List<String> adr;
