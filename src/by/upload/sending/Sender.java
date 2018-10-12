0d90922d6880f3338c6ea392a28e53dcd7317ce7
Sergey Patara <upload051@yahoo.com>
2018-10-12 10:39:35 +0300

    ...

--- src/by/upload/sending/Sender.java
+++ src/by/upload/sending/Sender.java
@@ -3,14 +3,16 @@ package by.upload.sending;
 import by.upload.parsing.AddressParser;
 import by.upload.parsing.TextParser;
 import by.upload.utils.Constants;
+import by.upload.utils.Utils;
 
 import java.io.IOException;
 import java.util.List;
 
 public class Sender {
     private Mailer mailer = new Mailer("anon1dvach@gmail.com", "ZAQ!@wsx");
-    private AddressParser main = new AddressParser();
-    private TextParser tx = new TextParser();
+    //private AddressParser main = new AddressParser();
+    //private TextParser tx = new TextParser();
+    private Utils utils = new Utils();
     private String text;
     private List<String> adr;
     private String subject = Constants.SUBJECT;
@@ -19,19 +21,11 @@ public class Sender {
     }
 
     {
-        try {
-            text = tx.mailText();
-        } catch (IOException e) {
-            System.out.println("No mail text");
-        }
+        text = utils.mailText();
     }
 
     {
-        try {
-            adr = main.mailAddress();
-        } catch (IOException e) {
-            System.out.println("No recipient addresses");
-        }
+        adr = utils.mailAddresses();
     }
 
 
--- src/by/upload/utils/Utils.java
+++ src/by/upload/utils/Utils.java
@@ -3,16 +3,37 @@ package by.upload.utils;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class Utils {
+    private String text;
+    private List<String> adr;
 
-    public static void writeToFile(String json) {
+    public static void writeToFile(String text) {
         String savePath = Constants.SAVEPATH;
         try {
-            Files.write(Paths.get(savePath), json.getBytes());
+            Files.write(Paths.get(savePath), text.getBytes());
             System.out.println("File saved. "+ savePath);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+    public String mailText(){
+        try {
+            text = new String(Files.readAllBytes(Paths.get(Constants.MAILTEXT)));
+        } catch (IOException e) {
+            System.out.println("File with text not found!");
+        }
+        return text;
+    }
+
+    public List<String> mailAddresses(){
+        try {
+            adr = Files.readAllLines(Paths.get(Constants.MAILADRES));
+        } catch (IOException e){
+            System.out.println("File with addresses not found!");
+        }
+        return adr;
+    }
+
 }
